086546ba0854f5fd5ed5c6c37359b286af5fd3dd
Woka Aditama <creaky.dinosaur@gmail.com>
2022-07-23 07:05:15 +0700

    development : create product module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module sirclo-ecommerce-task
+
+go 1.16
+
+require github.com/shopspring/decimal v1.3.1
---
+++ products/src/model/product.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"time"
+)
+
+// Product
+type Product struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Stock       int       `json:"stock"`
+	Price       float64   `json:"price"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+// Products type
+type Products []Product
+
+// NewProduct function for initialise Product model
+func NewProduct() *Product {
+	now := time.Now()
+	return &Product{
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+}
